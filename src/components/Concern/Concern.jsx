9e5a88855d8395196fc527a740c91df85789fc58
Shifatur-Rahman <rjnovel28@gmail.com>
2023-05-14 18:05:16 +0600

    fhedfsv nbdcvdv

--- src/components/Concern/Concern.jsx
+++ src/components/Concern/Concern.jsx
@@ -4,8 +4,11 @@ import "./Concern.css";
 const Concern = () => {
   return <>
 <h1>concern</h1>
-  
-  
+  <p> Talukder Foundry</p>
+  <p>Talukder uPVC</p>
+  <p>Talukder Distribution</p>
+  <p>Talukder Furniture</p>
+  <p>J & J Enterprise</p>
   </>;
 };
 
