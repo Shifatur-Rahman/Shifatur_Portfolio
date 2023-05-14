57088bf1695bf0abca780703288577fd42ddfc07
Shifatur-Rahman <rjnovel28@gmail.com>
2023-05-14 12:43:41 +0600

    reloader ughdcedghvdc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import AppRoute from "./router/AppRoute";
 import "./App.css";
+import React, {useState,useEffect} from "react"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById('spineer');
+  if(spinner){
+    setTimeout(()=>{
+spinner.style.display = 'none';
+setLoading(false);
+    },200)
+  }
   return (
     <>
+   {
+    !loading && (
       <AppRoute />
+    )
+   }
     </>
   );
 }
