da51f9545d6c188489f3ed625508013579259b07
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2023-07-04 18:10:27 +0600

    dcgfhvdfg

--- src/components/Career/Career.jsx
+++ src/components/Career/Career.jsx
@@ -14,10 +14,10 @@ const Career = () => {
       delay: 0, // values from 0 to 3000, with step 50ms
       easing: "ease", // default easing for AOS animations
       duration: 1500, // values from 0 to 3000, with step 50ms
-       disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+       disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or
        once: false, // whether animation should happen only once - while scrolling down
        mirror: false, // whether elements should animate out while scrolling past them
-       startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+       startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should 
        animatedClassName: 'aos-animate', // class applied on animation
        initClassName: 'aos-init', // class applied after initialization
        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
@@ -233,19 +233,12 @@ export default Career
 </div>
         </Accordion.Body>
       </Accordion.Item>
-
-
-      
       <Accordion.Item eventKey="4">
         <Accordion.Header>Tender Sales</Accordion.Header>
         <Accordion.Body>
        <p style={{color:"red"}}>No Post Avaiable at now</p>
         </Accordion.Body>
       </Accordion.Item>
-
-
-
-
 
 
     </Accordion>
