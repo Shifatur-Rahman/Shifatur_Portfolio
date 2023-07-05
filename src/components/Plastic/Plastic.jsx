4c7f529ff3166a9ac19b8995cd3af7c67c0ab505
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2023-07-05 14:44:31 +0600

    fdhgdshjsd

--- src/components/Plastic/Plastic.jsx
+++ src/components/Plastic/Plastic.jsx
@@ -11,45 +11,43 @@ const Plastic = () => {
 
     <Row>
 
-      <Col sm={12} md={12} lg={12} style={{marginTop:"4rem", textAlign:"justify"  }}>
-      <p>Now a days essence and use of plastic products knows no bound, 
-Plastic furniture becomes the first choice of mass people against
-their requirement, whether it is for our home patio, workplace or 
-even places such as restaurant and garden. Now, if you are 
-thinking that furniture made from plastic have cheap material 
-having lower mechanical & physical strength, then you are not 
-aware of all kinds of benefits associated with high quality plastic 
-furniture, which is not only long-lasting but elegant in looks. 
-The benefits of plastic furniture are given below:
-<br /> <br /> 
-<ul>
-  <li><a>Virtually maintenance free</a></li>
-  <li><a> Eco-friendly</a></li>
-  <li><a> Affordable yet stylish </a></li>
-  <li><a> Mobility </a></li>
-  <li><a> Long-lasting </a></li>
-  <li><a> Waterproof and safe under open sky </a></li>
-</ul>
-</p>
-      </Col>
+        <Col sm={12} md={12} lg={12} style={{ marginTop: "4rem", textAlign: "justify" }}>
+          <p>Now a days essence and use of plastic products knows no bound,
+            Plastic furniture becomes the first choice of mass people against
+            their requirement, whether it is for our home patio, workplace or
+            even places such as restaurant and garden. Now, if you are
+            thinking that furniture made from plastic have cheap material
+            having lower mechanical & physical strength, then you are not
+            aware of all kinds of benefits associated with high quality plastic
+            furniture, which is not only long-lasting but elegant in looks.
+            The benefits of plastic furniture are given below:
+            <br /> <br />
+            <ul>
+              <li><a>Virtually maintenance free</a></li>
+              <li><a> Eco-friendly</a></li>
+              <li><a> Affordable yet stylish </a></li>
+              <li><a> Mobility </a></li>
+              <li><a> Long-lasting </a></li>
+              <li><a> Waterproof and safe under open sky </a></li>
+            </ul>
+          </p>
+        </Col>
 
 
-      <Col sm={12} md={12} lg={12}>
-      
-      <ImageList  cols={3}>
-      {itemData.map((item) => (
-        <ImageListItem sx={{ height: '200px' }} key={item.img}>
-          <img
-            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
- 
-      </Col>
+        <Col sm={12} md={12} lg={12}>
+          <ImageList cols={4}>
+            {itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img style={{height:"350px"}}
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Col>
 
     </Row>
   </Container>
@@ -83,5 +81,13 @@ const itemData = [
   {
     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
-  }
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
 ];
