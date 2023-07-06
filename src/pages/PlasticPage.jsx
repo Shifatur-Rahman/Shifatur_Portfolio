d0f96b0c91e93e094a6b5eabcc95b2a1e27528ce
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2023-07-06 10:46:21 +0600

    hyggyuyug

--- src/pages/PlasticPage.jsx
+++ src/pages/PlasticPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TopNavigation from '../components/TopNavigation/TopNavigation'
 import PageTop from '../components/PageTop/PageTop'
 import Footer from '../components/Footer/Footer'
 import BackToTopButton from '../components/BackToTopButton/BackToTopButton'
 import Plastic from '../components/Plastic/Plastic'
+import { animateScroll } from "react-scroll";
 
 const PlasticPage = () => {
+  useEffect(() => {
+    animateScroll.scrollToTop({
+      duration: 300,
+      smooth: "easeInOutQuart",
+    });
+  }, []);
+  
   return (
     <>
     <TopNavigation title="Talukder Group" />
